bef5e6aafdbd18d859f0e2afb92e1f2d6dcc6a07
Riccardo Maifredi <r.maifredi@campus.unimib.it>
2025-07-17 22:49:12 +0200

    Workig on exercise 20

--- Notebooks/Exam/LIGO/read.py
+++ Notebooks/Exam/LIGO/read.py
@@ -1,15 +1,15 @@
 import h5py
 import numpy as np
 import sys
-sys.path.append(r"C:\Users\ricca\Documents\Unimib-Code\AstroStatistics\Notebooks\Exam\LIGO")
-from LIGO_Utils.LigoUtils import load_data, downsample_balanced, downsample_unbalanced
+sys.path.append(r"C:\Users\Phisio97\Documents\Astrostatistics\Notebooks\Exam")
+from LIGO.LIGO_Utils.LIGOUTILS import load_data, downsample_balanced, downsample_unbalanced
 
-data_path = r"C:\Users\ricca\Documents\Unimib-Code\AstroStatistics\Notebooks\Exam\LIGO\Npy\LIGO.h5"
+data_path = r"C:\Users\Phisio97\Downloads\LIGO.h5"
 
 y, data, keys = load_data(data_path)
 y, data = downsample_unbalanced(data, y, 100000, 42)
 
 print('Saving DS data...')
-np.save(r'C:\Users\ricca\Documents\Unimib-Code\AstroStatistics\Notebooks\Exam\LIGO\LIGO_v2\Npy\y.npy', y)
-np.save(r'C:\Users\ricca\Documents\Unimib-Code\AstroStatistics\Notebooks\Exam\LIGO\LIGO_v2\Npy\data.npy', data)
-np.save(r'C:\Users\ricca\Documents\Unimib-Code\AstroStatistics\Notebooks\Exam\LIGO\LIGO_v2\Npy\keys.npy', keys)
+np.save(r'C:\Users\Phisio97\Documents\Astrostatistics\Notebooks\Exam\LIGO\LIGO_v2\Npy\y.npy', y)
+np.save(r'C:\Users\Phisio97\Documents\Astrostatistics\Notebooks\Exam\LIGO\LIGO_v2\Npy\data.npy', data)
+np.save(r'C:\Users\Phisio97\Documents\Astrostatistics\Notebooks\Exam\LIGO\LIGO_v2\Npy\keys.npy', keys)
